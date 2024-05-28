2518676824fed060d56cfd87eed395e9ab7b46a3
Trevor <Trevormahinda@gmail.com>
2024-05-28 19:21:49 +0300

    vizuri

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -40,25 +40,25 @@ def delete_place(place_id):
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)  # noqa
 def create_place(city_id):
-    """creates a place object"""
+    """Creates a Place object"""
     city = storage.get("City", city_id)
     if not city:
         abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
     data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
     if 'user_id' not in data:
         abort(400, 'Missing user_id')
+    user = storage.get("User", data["user_id"])
+    if not user:
+        abort(404)
     if 'name' not in data:
         abort(400, 'Missing name')
-    user = storage.get("User", data['user_id'])
-    if not user:
-        abort(400)
     place = Place(**data)
     setattr(place, 'city_id', city_id)
     storage.new(place)
     storage.save()
-    return (jsonify(place.to_dict()), 201)
+    return make_response(jsonify(place.to_dict()), 201)
 
 
 @app_views.route('/places/<place_id>', methods=["PUT"], strict_slashes=False)  # noqa
