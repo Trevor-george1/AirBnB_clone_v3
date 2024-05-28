8aee8ec71882892a9016815ebad4e406467d1cea
Trevor <Trevormahinda@gmail.com>
2024-05-28 11:54:22 +0300

    update states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -57,6 +57,10 @@ def create_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """updates a state object by ID"""
+    if request.content_type != 'application/json':
+        abort(400, "Not a JSON")
+    if not request.get_json():
+        abort(400, "Not a JSON")
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
